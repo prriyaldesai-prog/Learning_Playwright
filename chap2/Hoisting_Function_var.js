40fd880a432a3f414cb88dbb04261ba6f6195b3c
prriyaldesai-prog <prriyaldesai@gmail.com>
2026-02-17 11:17:10 +0530

    Add Hoisting examples with functions

---
+++ chap2/Hoisting_Function_var.js
@@ -0,0 +1,13 @@
+//function definition
+function getuserstatus() {
+    console.log(status_code);
+    var status_code = 200;
+    console.log(status_code);
+}
+
+//function call
+getuserstatus();
+
+
+
+// var gives undefined
--- chap2/Hoisting_var.js
+++ chap2/Hoisting_var.js
@@ -1,4 +1,15 @@
-console.log(a);
-var a = 10;
+// console.log(a);
+// var a = 10;
+
+// console.log(a);
+
+
+
+console.log(browserName);
+var browserName = "chrome";
+
+
+//TDZ_Temporal Dead Zone
 
 console.log(a);
+var a = "Playwright";
