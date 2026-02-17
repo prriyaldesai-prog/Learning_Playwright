c137fa588800cc23eb1e60f8de03f304d16e22c0
prriyaldesai-prog <prriyaldesai@gmail.com>
2026-02-17 08:43:18 +0530

    Add Hoisting examples

---
+++ chap2/Hoisting_let.js
@@ -0,0 +1,9 @@
+console.log(a);
+let a = 10;
+console.log(a);
+
+
+
+// let gives reference error where in hoisting we use variable before declaration
+// shortcut for commenting in win is - select to comment and ctrl+/
+
